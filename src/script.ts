ec260634f61b18aefacb986e3a3a8cc1a7e6b1d4
hyoban <hi@hyoban.cc>
2022-07-25 17:06:46 +0800

    refactor: use cac for cli

--- src/script.ts
+++ src/script.ts
@@ -1,12 +1,12 @@
-import { readFile, readFileSync, writeFile, writeFileSync } from 'fs'
+import { readFile, writeFile } from 'fs/promises'
 // @ts-expect-error no type information available for module
 import { parse, stringify } from 'opml'
 import Parser from 'rss-parser'
-import * as dotenv from 'dotenv'
+import { cac } from 'cac'
 import type { Feeds, Sub } from './types'
 import { Type, Version } from './types'
-dotenv.config()
 
+// Get the current number of feeds
 const countFeeds = (subs: Sub[] | undefined) => {
   let count = 0
   subs?.forEach((sub) => {
@@ -18,6 +18,7 @@ const countFeeds = (subs: Sub[] | undefined) => {
   return count
 }
 
+// Get all feeds info
 const getAllFeeds = (subs: Sub[] | undefined) => {
   let feeds: Sub[] = []
   subs?.forEach((sub) => {
@@ -31,93 +32,129 @@ const getAllFeeds = (subs: Sub[] | undefined) => {
 
 const parser = new Parser()
 
-const parseAndWrite = async (feedUrl: string, folder: string) => {
+const parseAndWrite = async (feedUrl: string, folder?: string, name?: string) => {
   const feed = await parser.parseURL(feedUrl)
   // eslint-disable-next-line no-console
   console.log(`Adding ${feed.title}`)
-  const opmlFile = readFileSync('./feeds.opml', 'utf8')
-  parse(opmlFile, (_err: any, opmlDoc: Feeds) => {
-    if (
-      getAllFeeds(opmlDoc.opml.body.subs).some(item => item.xmlUrl === feedUrl)
-    ) {
-      console.error(`${feed.title} already exists`)
-      return
-    }
-    const newOutline: Sub = {
-      text: feed.title || '',
-      title: feed.title || '',
-      description: feed.description || '',
-      type: Type.RSS,
-      version: Version.RSS,
-      xmlUrl: feedUrl,
-      htmlUrl: feed.link,
-    }
-    if (folder) {
-      const folderIndex = opmlDoc.opml.body.subs.findIndex(
-        item => item.text === folder && item.subs,
-      )
-      if (folderIndex === -1) {
-        console.error(`Folder ${folder} not found`)
+  const opmlFile = await readFile('./feeds.opml', { encoding: 'utf8' })
+  parse(opmlFile, async (err: any, theOutline: Feeds) => {
+    if (!err) {
+      if (
+        getAllFeeds(theOutline.opml.body.subs).some(item => item.xmlUrl === feedUrl)
+      ) {
+        console.error(`${feed.title} already exists`)
         return
       }
-      opmlDoc.opml.body.subs[folderIndex].subs?.push(newOutline)
+      const newOutline: Sub = {
+        text: name ?? feed.title ?? '',
+        title: name ?? feed.title ?? '',
+        description: feed.description ?? '',
+        type: Type.RSS,
+        version: Version.RSS,
+        xmlUrl: feedUrl,
+        htmlUrl: feed.link,
+      }
+      if (folder) {
+        const folderIndex = theOutline.opml.body.subs.findIndex(
+          item => item.text === folder && item.subs,
+        )
+        if (folderIndex === -1) {
+          console.error(`Folder ${folder} not found`)
+          return
+        }
+        theOutline.opml.body.subs[folderIndex].subs?.push(newOutline)
+      }
+      else {
+        theOutline.opml.body.subs.push(newOutline)
+      }
+      const newOpml = stringify(theOutline)
+      await writeFile('./feeds.opml', newOpml)
     }
     else {
-      opmlDoc.opml.body.subs.push(newOutline)
+      console.error('Parse opml file error', err)
     }
-    const newOpml = stringify(opmlDoc)
-    writeFileSync('./feeds.opml', newOpml)
   })
 }
 
-const newFeed = process.argv.slice(2)
-if (newFeed.length !== 0) {
-  // add a new feed to the opml file
-  const [shortcode, feedUrl, folder] = newFeed
-  // custom your own shortcode
+// opml to json
+async function writeOpmlToJson() {
+  try {
+    const opmltext = await readFile('./feeds.opml', { encoding: 'utf8' })
+    parse(opmltext, async (err: any, theOutline: any) => {
+      // eslint-disable-next-line no-console
+      console.log('Total feeds:', countFeeds(theOutline.opml.body.subs))
+      if (!err) {
+        await writeFile(
+          './feeds.json',
+          JSON.stringify(theOutline, undefined, 4),
+        )
+      }
+      else {
+        console.error('Parse opml file error', err)
+      }
+    })
+  }
+  catch (err) {
+    console.error('Load opml file error', err)
+  }
+}
+
+// custom your own shortcode here
+async function handleShortCode(shortcode: string, feedUrl: string, folder: string, name: string) {
   if (shortcode === 'b') {
-    parseAndWrite(`https://rsshub.app/bilibili/user/dynamic/${feedUrl}`, '视频')
+    await parseAndWrite(`https://rsshub.app/bilibili/user/dynamic/${feedUrl}`, '视频')
   }
   else if (shortcode === 'ab') {
     parser.parseURL(`http://rsshub.hyoban.cc:1200/bilibili/user/followings/${feedUrl}`, (_err, feed) => {
-      feed.items.forEach((item) => {
-        parseAndWrite(`https://rsshub.app/bilibili/user/dynamic/${item.link?.split('/').pop()}`, '视频')
+      feed.items.forEach(async (item) => {
+        await parseAndWrite(`https://rsshub.app/bilibili/user/dynamic/${item.link?.split('/').pop()}`, '视频')
       })
     })
   }
   else if (shortcode === 't') {
-    parseAndWrite(`https://rsshub.app/twitter/user/${feedUrl}`, '时间线')
+    await parseAndWrite(`https://rsshub.app/twitter/user/${feedUrl}`, '时间线')
   }
   else if (shortcode === 'w') {
-    parseAndWrite(`http://rsshub.hyoban.cc:1200/weibo/user/${feedUrl}`, '时间线')
+    await parseAndWrite(`http://rsshub.hyoban.cc:1200/weibo/user/${feedUrl}`, '时间线')
   }
-  else {
-    parseAndWrite(feedUrl, folder)
+  else if (shortcode === 'g') {
+    await parseAndWrite(`https://github.com/${feedUrl}.atom`, 'GitHub', feedUrl)
+  }
+  else if (shortcode === 'default') {
+    await parseAndWrite(feedUrl, folder === 'default' ? undefined : folder, name === 'default' ? undefined : name)
   }
 }
-else {
-  // opml to json
-  readFile('./feeds.opml', (err, opmltext) => {
-    if (!err) {
-      parse(opmltext, (err: any, theOutline: any) => {
-        // eslint-disable-next-line no-console
-        console.log('total feeds:', countFeeds(theOutline.opml.body.subs))
-        if (!err) {
-          writeFile(
-            './feeds.json',
-            JSON.stringify(theOutline, undefined, 4),
-            (err) => {
-              if (!err)
-                // eslint-disable-next-line no-console
-                console.log('Successfully written to file')
-            },
-          )
-        }
-      })
-    }
-    else {
-      console.error(err)
-      process.exit(1)
-    }
+
+const cli = cac()
+
+cli
+  .command('add <...url>', 'Add a new feed', {})
+  .option('-s, --short <code>', 'Shortcode for the feed', {
+    default: 'default',
   })
-}
+  .option('-f, --folder <foler>', 'Provide a folder for feed', {
+    default: 'default',
+  })
+  .option('-n, --name <name>', 'Provide a name for feed', {
+    default: 'default',
+  })
+  .action(async (urls, options) => {
+    const shortcode = options.short as string
+    const folder = options.folder as string
+    const name = options.name as string
+    const feedUrls = urls as string[]
+
+    feedUrls.forEach(async (feedUrl) => {
+      await handleShortCode(shortcode, feedUrl, folder, name)
+    })
+  })
+
+cli
+  .command('wirte', 'Write opml to json', {})
+  .action(async () => {
+    await writeOpmlToJson()
+  })
+
+cli.help()
+
+cli.parse()
